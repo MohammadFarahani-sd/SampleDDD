f019c53cccf98112113dcdad60cf603b28372a52
Farahani <Farahani.Mohammad@Hasti.co>
2024-01-10 15:59:47 +0330

    some other configuration for fleet provider

---
+++ src/Infrastructure/EntityConfigurations/Customers/FleetProviderEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using FleetProvider.Domain.AggregatesModel.FleetProviderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FleetProvider.Infrastructure.EntityConfigurations.Customers;
+
+public class FleetShiftEntityTypeConfiguration : EntityTypeConfiguration<FleetShift>
+{
+    public override void ConfigureDerived(EntityTypeBuilder<FleetShift> configuration)
+    {
+        configuration.ToTable("FleetShift", "FP");
+
+        configuration.Property(o => o.Id);
+
+        configuration.HasKey(o => o.Id);
+    }
+}
